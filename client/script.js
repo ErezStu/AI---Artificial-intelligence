2d90ed4e774c6e6e5821712d503f6134816e679d
erez stozenko <erezstozenko@gmail.com>
2023-02-11 20:05:25 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // Data from the server - Open API AI response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://ai-artificial-intelligence.onrender.com", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
